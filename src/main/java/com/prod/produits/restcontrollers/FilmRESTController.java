9df4bf2780f48d25d435ee3f458630ce0cf0ff9a
DorraNsir <dorransir24@gmail.com>
2024-10-09 10:59:32 +0200

    Create the REST web service for ProduitFilm CRUD

--- src/main/java/com/prod/produits/restcontrollers/FilmRESTController.java
+++ src/main/java/com/prod/produits/restcontrollers/FilmRESTController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,29 @@ public class FilmRESTController {
     public ProduitFilm getProduitById(@PathVariable("id") Long id){
         return produitService.getProduit(id);
     }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ProduitFilm creaProduitFilm(@RequestBody ProduitFilm produitFilm){
+        return produitService.saveProduitFilm(produitFilm);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT)
+    public ProduitFilm updaProduitFilm(@RequestBody ProduitFilm produitFilm){
+        return produitService.updateProduitFilm(produitFilm);
+    }
+
+    @RequestMapping(value = "/{id}",method=RequestMethod.DELETE)
+    public void deleProduitFilm(@PathVariable("id") Long id){
+         produitService.deleteProduitById(id);
+    }
+
+    @RequestMapping(value = "/genre/{idG}",method=RequestMethod.GET)
+    public List<ProduitFilm> findByGenreFilmIdG(@PathVariable("idG") Long idG){
+         return produitService.findByGenreFilmIdG(idG);
+    }
+
+
+
 
 
 
